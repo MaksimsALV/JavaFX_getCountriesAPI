45b0c9623582e30d4db1fee19cde294ec731fa51
MaksimsALV <maxim.ahmetov@gmail.com>
2025-03-16 16:26:07 +0200

    adding getCountryData method +UI improvements

--- src/com/restcountries/api/API.java
+++ src/com/restcountries/api/API.java
@@ -1,7 +1,5 @@
 package com.restcountries.api;
 
-import com.restcountries.extractor.Extractor;
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -43,5 +41,39 @@ public class API {
             e.printStackTrace();
         }
         return countryNames;
+    }
+
+    public static List<String> getCountryData(String selectedCountry) {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        String countryEndpoint = "https://restcountries.com/v3.1/name/" + selectedCountry;
+
+        //String capitalEndpoint = "https://restcountries.com/v3.1/capital/{capital}"; //todo need to add this later. I want to call country name first -> get capital name from there to UI -> call the capital using name from UI
+        //todo end
+
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(URI.create(countryEndpoint)) //sets url
+                .GET() //sets GET request method
+                .build(); //executing the request (mandatory step)
+
+        List<String> countryData = new ArrayList<>();
+        try { //todo add validation to 200, else error
+            HttpResponse<String> getResponse = httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
+            String responseBody = getResponse.body();
+
+            //parse JSON String into proper JSON Object using JSONObject library (pretty print indicator 4)
+            //apparently response was bricking due to JSON objects and arrays mix, so I had to add if/else statement to handle both cases
+            if (responseBody.startsWith("[")) {
+                JSONArray countryArray = new JSONArray(responseBody);
+                for (int i = 0; i < countryArray.length(); i++) {
+                    countryData.add(countryArray.getJSONObject(i).toString(4));
+                }
+            } else {
+                countryData.add(new JSONObject(responseBody).toString(4));
+            }
+            System.out.println(countryData); //analysis for the output
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return countryData;
     }
 }
--- src/com/restcountries/application/Controller.java
+++ src/com/restcountries/application/Controller.java
@@ -6,6 +6,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,18 +15,38 @@ import java.util.stream.Collectors;
 public class Controller {
 
     @FXML
-    private Button run;
+    private Button tryMe;
 
     @FXML
     private ListView<String> countryNameList;
 
-    public void buttonClick() {
+    @FXML
+    private Button exportToJSON;
+
+    @FXML
+    private Button getCapitalData;
+
+    @FXML
+    private Button getCountryData;
+
+    @FXML
+    private TextArea capitalData;
+
+    @FXML
+    private TextArea countryData;
+
+    public void tryMeClick() { //this calls my API getCountryNames method from API class, then filters endresult from there and returns as ObservableList
         List<String> countries = API.getCountryNames();
         List<String> sortByName = countries.stream()
                 .sorted()
                 .collect(Collectors.toList());
         ObservableList<String> countryNamesList = FXCollections.observableArrayList(sortByName);
         countryNameList.setItems(countryNamesList);
+    }
+
+    public void getCountryDataClick() {
+        String selectedCountry = countryNameList.getSelectionModel().getSelectedItem();
+        API.getCountryData(selectedCountry);
     }
 
 }
